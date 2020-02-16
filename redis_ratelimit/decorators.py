5ed7d249db193a7f7c4b81d72af14dd1b87cc959
PredaMart <predaamart@outlook.com>
2020-02-16 23:09:08 +0100

    Adapt uses.

--- redis_ratelimit/decorators.py
+++ redis_ratelimit/decorators.py
@@ -4,14 +4,19 @@ from redis_ratelimit.exceptions import RateLimited
 from redis_ratelimit.utils import is_rate_limited, default_pool
 
 
-def ratelimit(rate, key, redis_pool: dict = default_pool):
-    def decorator(func):
-        @wraps(func)
-        async def decorated_function(*args, **kwargs):
-            if is_rate_limited(rate, key, func, redis_pool):
-                raise RateLimited("Too Many Requests")
-            return await func(*args, **kwargs)
+# def _ratelimit(rate: str, key: str, redis_pool: dict = default_pool):
+#     def decorator(func):
+#         @wraps(func)
+#         async def decorated_function(*args, **kwargs):
+#             if is_rate_limited(rate, key, func, redis_pool):
+#                 raise RateLimited("Too Many Requests")
+#             return await func(*args, **kwargs)
 
-        return decorated_function
+#         return decorated_function
 
-    return decorator
+#     return decorator
+
+
+def ratelimit(rate: str, key: str, value: str, redis_pool: dict = default_pool):
+    check = is_rate_limited(rate, key, value, redis_pool)
+    return check
--- redis_ratelimit/utils.py
+++ redis_ratelimit/utils.py
@@ -8,7 +8,7 @@ rate_re = re.compile(r"([\d]+)/([\d]*)([smhd])")
 UNITS = {"s": 1, "m": 60, "h": 60 * 60, "d": 24 * 60 * 60}
 
 
-def parse_rate(rate):
+def parse_rate(rate: str):
     try:
         count, factor, unit = rate_re.match(rate).groups()
         count = int(count)
@@ -20,23 +20,23 @@ def parse_rate(rate):
         raise RateLimiterException("Invalid rate value")
 
 
-def is_rate_limited(rate, key, func, redis_pool):
+def is_rate_limited(rate: str, key: str, value: str, redis_pool: dict):
     if not rate:
-        return False
+        return (None, None)
 
     count, seconds = parse_rate(rate)
-    redis_key = "{}/{}/{}/{}".format(key, func.__name__, count, seconds)
+    redis_key = f"[{key}][{value}][{count}/{seconds}]"
 
-    r = redis.Redis(redis_pool)
+    r = redis.Redis(**redis_pool)
 
     current = r.get(redis_key)
     if current:
         current = int(current.decode("utf-8"))
         if current >= count:
-            return True
+            return (True, (count - (current if current else 0), count))
 
     value = r.incr(redis_key)
     if value == 1:
         r.expire(redis_key, seconds)
 
-    return False
+    return (False, (count - (current if current else 0), count))
